128ac5a49ab3a36ddd58cd61ef2f9d4fcd7283a9
Bohdan <60944532+BohdanNikolaychuk@users.noreply.github.com>
2023-01-25 08:18:39 +0200

    Update asyncAction.ts

--- src/store/article/asyncAction.ts
+++ src/store/article/asyncAction.ts
@@ -5,7 +5,7 @@ export const FetchAllArticles = createAsyncThunk(
   'arctile/fetchArctile',
   async function (start: number = 0, { rejectWithValue }) {
     try {
-      const response = await axios.get(`/articles?_start=${start}&_limit=9`);
+      const response = await axios.get(`/articles?_start=${start}&_limit=100`);
 
       return response.data;
     } catch (err: any) {
